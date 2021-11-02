1eb77f9c91a42abf5d70263f52113262176f3f46
Valdenir Severino <vald3nir@gmail.com>
2021-11-02 10:48:00 -0300

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,26 @@
+// ------------------------------------------------------------------
+// APP CONFIGURATION
+// ------------------------------------------------------------------
+
+module.exports = {
+  logging: true,
+
+  intentMap: {
+    'AMAZON.StopIntent': 'END',
+    'AMAZON.YesIntent': 'YesIntent',
+    'AMAZON.NoIntent': 'NoIntent',
+    'AMAZON.HelpIntent': 'HelpIntent',
+    'AMAZON.StopIntent': 'StopIntent',
+    'AMAZON.CancelIntent': 'StopIntent'
+  },
+
+  intentsToSkipUnhandled: [
+    'END', 'StopIntent'
+  ],
+
+  db: {
+    FileDb: {
+      pathToFile: '../db/db.json',
+    },
+  },
+};
